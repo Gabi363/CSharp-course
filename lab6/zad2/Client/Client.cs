e7ef601ee8430df1e545054ee2c5a57d0bf9a385
Gabriela Żmuda <115559396+Gabi363@users.noreply.github.com>
2024-04-18 10:15:52 +0200

    add zad2

---
+++ lab6/zad2/Client/Client.cs
@@ -0,0 +1,43 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+public class Client
+{
+    public static void Main()
+    {
+        IPHostEntry host = Dns.GetHostEntry("localhost");
+        IPAddress ipAddress = host.AddressList[0];
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+
+        Socket socket = new(
+            localEndPoint.AddressFamily, 
+            SocketType.Stream, 
+            ProtocolType.Tcp);
+        socket.Connect(localEndPoint);
+        Console.WriteLine("Połączenie");
+
+
+        Console.WriteLine("Podaj wiadomość dla serwera:");
+        string message = Console.ReadLine();
+        byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+        byte[] sizeBytes = BitConverter.GetBytes(messageBytes.Length);
+        socket.Send(sizeBytes, SocketFlags.None);
+        socket.Send(messageBytes, SocketFlags.None);
+
+        var sizeBufor = new byte[4];
+        socket.Receive(sizeBufor, 4, SocketFlags.None);
+        int bytes = BitConverter.ToInt32(sizeBufor, 0);
+        var bufor = new byte[bytes];
+        int amountBytes = socket.Receive(bufor, SocketFlags.None);
+        String messageServer = Encoding.UTF8.GetString(bufor, 0, amountBytes);
+        Console.WriteLine("Serwer: " + messageServer);
+
+        try {
+            socket.Shutdown(SocketShutdown.Both);
+            socket.Close();
+        }
+        catch{}
+    }
+}
---
+++ lab6/zad2/Server/Server.cs
@@ -0,0 +1,45 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+public class Server
+{
+    public static void Main()
+    {
+        IPHostEntry host = Dns.GetHostEntry("localhost");
+        IPAddress ipAddress = host.AddressList[0];
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+
+        Socket socketServer = new(
+            localEndPoint.AddressFamily,
+            SocketType.Stream,
+            ProtocolType.Tcp);
+        socketServer.Bind(localEndPoint);
+        socketServer.Listen(100);
+        Console.WriteLine("Nasłuchiwanie");
+
+        Socket socketClient = socketServer.Accept();
+        Console.WriteLine("Połączenie");
+
+        var sizeBufor = new byte[4];
+        socketClient.Receive(sizeBufor, 4, SocketFlags.None);
+        int bytes = BitConverter.ToInt32(sizeBufor);
+        byte []bufor = new byte[bytes];
+        int received = socketClient.Receive(bufor, SocketFlags.None);
+        String messageClient = Encoding.UTF8.GetString(bufor, 0, received);
+        Console.WriteLine("Klient: " + messageClient);
+
+        string answer = "odczytałem: " + messageClient;
+        var echoBytes = Encoding.UTF8.GetBytes(answer);
+        byte[] sizeBytes = BitConverter.GetBytes(echoBytes.Length);
+        socketClient.Send(sizeBytes, SocketFlags.None);
+        socketClient.Send(echoBytes, 0);
+
+        try {
+            socketServer.Shutdown(SocketShutdown.Both);
+            socketServer.Close();
+        }
+        catch{}
+    }
+}
